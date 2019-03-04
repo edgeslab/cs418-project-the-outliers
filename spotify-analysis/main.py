a6544648ae4e97e3f920a4d002a836357eae9d49
seehamrun <sihamh@google.com>
2019-03-03 20:46:22 -0600

    add function to get list of tracks out of the playlist track object and documentation

--- spotify-analysis/main.py
+++ spotify-analysis/main.py
@@ -43,14 +43,39 @@ def get_playlist(playlist_id, access_token):
         query (string): the playlist_id to get
 
     Returns:
-        the contents of the playlist?
+        A playlist object from spotify, see speck here
+        https://developer.spotify.com/documentation/web-api/reference/object-model/#playlist-object-simplified
     """
     url = "https://api.spotify.com/v1/playlists/" + playlist_id
     headers = {'Authorization': 'Bearer ' + access_token}
     response = requests.get(url, headers=headers)
 
     json = response.json()
-    print(json)
+    return json
+
+def get_playlist_tracks(playlist_object):
+    """
+    Gets the tracks in the playlist object specified
+
+    Args:
+        playlist_object(dictionary): https://developer.spotify.com/documentation/web-api/reference/object-model/#playlist-object-simplified
+    
+    Returns:
+        a list of the tracks in that object
+    """
+
+    # all_tracks is a paging object: 
+    # https://developer.spotify.com/documentation/web-api/reference/object-model/#paging-object
+    all_tracks = playlist_object["tracks"]
+
+    # the items in the object are playlist track objects: 
+    # https://developer.spotify.com/documentation/web-api/reference/object-model/#playlist-track-object
+    tracks = []
+    for t in all_tracks["items"]:
+        track = t["track"]
+        tracks.append(track)
+        print(track["name"])
+    return tracks
 
 
 print(api_key.client_key)
@@ -58,4 +83,5 @@ print(api_key.client_secret)
 access_token = get_access_token()
 
 # Rap Caviar Playlist
-get_playlist("37i9dQZF1DX0XUsuxWHRQd", access_token)
+rap_caviar_playlist = get_playlist("37i9dQZF1DX0XUsuxWHRQd", access_token)
+rap_caviar_tracks = get_playlist_tracks(rap_caviar_playlist)
