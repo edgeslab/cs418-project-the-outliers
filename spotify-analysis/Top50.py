35da49c72eeee374d3d02a383faac61681161c8a
Siham Hussein <seehamrun@users.noreply.github.com>
2019-03-04 19:11:33 -0600

    move top50 csv into a separate directory, update GetTop50.py to only do the file mapping csvs and Top50.py to contain the WIP (#7)

---
+++ spotify-analysis/Top50.py
@@ -0,0 +1,187 @@
+import main
+import time
+
+access_token = main.get_access_token()
+
+CzechRepublicTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbIP3c3fqVrJY", access_token)
+time.sleep(0.200)
+
+DenmarkTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbL3J0k32lWnN", access_token)
+time.sleep(0.200)
+
+DominicanRepublicTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKAbrMR8uuf7", access_token)
+time.sleep(0.200)
+
+EcuadorTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJlM6nvL1nD1", access_token)
+time.sleep(0.200)
+
+ElSalvadorTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLxoIml4MYkT", access_token)
+time.sleep(0.200)
+
+EstoniaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLesry2Qw2xS", access_token)
+time.sleep(0.200)
+
+FinlandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbMxcczTSoGwZ", access_token)
+time.sleep(0.200)
+
+FranceTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbIPWwFssbupI", access_token)
+time.sleep(0.200)
+
+GermanyTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJiZcmkrIHGU", access_token)
+time.sleep(0.200)
+
+GreeceTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJqdarpmTJDL", access_token)
+time.sleep(0.200)
+
+GuatemalaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLy5tBFyQvd4", access_token)
+time.sleep(0.200)
+
+HondurasTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJp9wcIM9Eo5", access_token)
+time.sleep(0.200)
+
+HongKongTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLwpL8TjsxOG", access_token)
+time.sleep(0.200)
+
+HungaryTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbNHwMxAkvmF8", access_token)
+time.sleep(0.200)
+
+IcelandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKMzVsSGQ49S", access_token)
+time.sleep(0.200)
+
+IndonesiaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbObFQZ3JLcXt", access_token)
+time.sleep(0.200)
+
+IrelandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKM896FDX8L1", access_token)
+time.sleep(0.200)
+
+ItalyTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbIQnj7RRhdSX", access_token)
+time.sleep(0.200)
+
+JapanTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKXQ4mDTEBXq", access_token)
+time.sleep(0.200)
+
+LavtiaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJWuzDrTxbKS", access_token)
+time.sleep(0.200)
+
+LithuaniaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbMx56Rdq5lwc", access_token)
+time.sleep(0.200)
+
+LuxembourgTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKGcyg6TFGx6", access_token)
+time.sleep(0.200)
+
+MalaysiaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJlfUljuZExa", access_token)
+time.sleep(0.200)
+
+MexicaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbO3qyFxbkOE1", access_token)
+time.sleep(0.200)
+
+NetherlandsTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKCF6dqVpDkS", access_token)
+time.sleep(0.200)
+
+NewZealandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbIPWwFssbupI", access_token)
+time.sleep(0.200)
+
+NicaraguaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbISk8kxnzfCq", access_token)
+time.sleep(0.200)
+
+NorwayTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJvfa0Yxg7E7", access_token)
+time.sleep(0.200)
+
+PanamaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKypXHVwk1f0", access_token)
+time.sleep(0.200)
+
+PeruTopTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJfdy5b0KP7W", access_token)
+time.sleep(0.200)
+
+PhilippinesTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbNBz9cRCSFkY", access_token)
+time.sleep(0.200)
+
+PolandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbN6itCcaL3Tt", access_token)
+time.sleep(0.200)
+
+PortugalTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKyJS56d1pgi", access_token)
+time.sleep(0.200)
+
+RomaniaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbNZbJ6TZelCq", access_token)
+time.sleep(0.200)
+
+SingaporeTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbK4gjvS1FjPY", access_token)
+time.sleep(0.200)
+
+SlovakiaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbKIVTPX9a2Sb", access_token)
+time.sleep(0.200)
+
+SouthAfricaTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbMH2jvi6jvjk", access_token)
+time.sleep(0.200)
+
+SpainTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbNFJfN1Vw8d9", access_token)
+time.sleep(0.200)
+
+SwedenTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLoATJ81JYXz", access_token)
+time.sleep(0.200)
+
+SwitzerlandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbJiyhoAPEfMK", access_token)
+time.sleep(0.200)
+
+TaiwanTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbMnZEatlMSiu", access_token)
+time.sleep(0.200)
+
+ThailandTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbMnz8KIWsvf9", access_token)
+time.sleep(0.200)
+
+TurkeyTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbIVYVBNw9D5K", access_token)
+time.sleep(0.200)
+
+UnitedKingdomTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLnolsZ8PSNw", access_token)
+time.sleep(0.200)
+
+UruguayTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbMJJi3wgRbAy", access_token)
+time.sleep(0.200)
+
+VietnamTop50 = main.get_playlist_audio_features(
+    "37i9dQZEVXbLdGSmz6xilI", access_token)
