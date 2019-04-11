8fe65015d3c2be21ceb9b246f3e5784bcef458e3
seehamrun <sihamh@google.com>
2019-04-11 13:56:29 -0700

    update some of the visualization to include a draft of the yearly data

--- spotify-analysis/visualization.py
+++ spotify-analysis/visualization.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import GetTopYearly
 
+def get_csv(filename):
+    return pd.read_csv(filename)
 
-def getCSV(country):
+def get_country_csv(country):
     """
     Opens the Top50.csv file for the country provided.
 
@@ -13,8 +16,18 @@ def getCSV(country):
     Returns:
         pandas dataset
     """
-    return pd.read_csv('top50csv/' + country + "Top50.csv")
+    return get_csv('top50CountryCSV/' + country + "Top50.csv")
 
+def get_yearly_csv(year):
+    """
+    Opens the TopTracks.csv file for the year provided.
+
+    Args:
+        country(string): the year to open
+    Returns:
+        pandas dataset
+    """
+    return get_csv('topTracksYearsCSV/' + year + "TopTracks.csv")
 
 def makeDanceabilityBarPlot(countrya, countryb):
     """
@@ -24,8 +37,8 @@ def makeDanceabilityBarPlot(countrya, countryb):
     Args:
         countrya, countryb (string): the two countries to compare
     """
-    countryAcsv = getCSV(countrya)
-    countryBcsv = getCSV(countryb)
+    countryAcsv = get_country_csv(countrya)
+    countryBcsv = get_country_csv(countryb)
     sns.distplot(countryAcsv['danceability'], label=countrya + ' Danceability',)
     sns.distplot(countryBcsv['danceability'], label=countryb + ' Danceability')
     plt.title(countrya + ' VS ' + countryb + ' danceability')
@@ -42,11 +55,28 @@ def makeLoudnessBarplot(countrya, countryb):
     Args:
         countrya, countryb (string): the two countries to compare
     """
-    countryAcsv = getCSV(countrya)
-    countryBcsv = getCSV(countryb)
+    countryAcsv = get_country_csv(countrya)
+    countryBcsv = get_country_csv(countryb)
     sns.distplot(countryAcsv['loudness'], label=countrya + ' Loudness')
     sns.distplot(countryBcsv['loudness'], label=countryb + ' Loudness')
     plt.title(countrya + ' VS ' + countryb + ' loudness')
     plt.xlabel('Loudness')
     plt.legend()
     plt.show()
+
+def create_features_dotplot(featurea, featureb):
+    """
+    Creates a dot plot outline of the features provided for all of the yearly data
+    """
+    all_data = pd.DataFrame()
+    for year in GetTopYearly.top_playlists_per_year:
+        year_pd = get_yearly_csv(year)
+        year_pd["year"] = year
+        all_data = pd.concat([year_pd, all_data])
+        
+    sns.set(style="whitegrid")
+    sns.stripplot(x=featurea, y=featureb, data=all_data, hue="year")
+    plt.legend()
+    plt.show()
+
+create_features_dotplot("loudness", "danceability")
