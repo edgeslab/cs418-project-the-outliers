9a098078247999fd387e997bbf9af2fd0b7c7897
Amit Panthi <47650855+apanth2@users.noreply.github.com>
2019-04-11 20:08:20 -0600

    Add files via upload

---
+++ spotify-analysis/Info_Details.py
@@ -0,0 +1,318 @@
+import pandas as pd
+
+
+
+df01 = pd.read_csv("ArgentinaTop50.csv")
+df01.info()
+df01.describe()
+
+
+df2 = pd.read_csv("AustraliaTop50.csv")
+df2.info()
+df2.describe()
+
+
+
+
+df3 = pd.read_csv("AustriaTop50.csv")
+df3.info()
+df3.describe()
+
+
+df4 = pd.read_csv("BelgiumTop50.csv")
+df4.info()
+df4.describe()
+
+
+
+df5 = pd.read_csv("BoliviaTop50.csv")
+df5.info()
+df5.describe()
+
+
+
+df6 = pd.read_csv("BrazilTop50.csv")
+df6.info()
+df6.describe()
+
+
+
+df7 = pd.read_csv("BulgariaTop50.csv")
+df7.info()
+df7.describe()
+
+
+df8 = pd.read_csv("CanadaTop50.csv")
+df8.info()
+df8.describe()
+
+
+df9 = pd.read_csv("ChilleTop50.csv")
+df9.info()
+df9.describe()
+
+
+df10 = pd.read_csv("ColumbiaTop50.csv")
+df10.info()
+df10.describe()
+
+df11 = pd.read_csv("CzechRepublicTop50.csv")
+df11.info()
+df11.describe()
+
+
+df12 = pd.read_csv("DenmarkTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+
+df12 = pd.read_csv("DominicanRepublicTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("EcuadorTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("ElSalvadorTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("EstoniaTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("FinlandTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("FranceTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("GermanyTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("GreeceTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("GuatemalaTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("HondurasTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("HongKongTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("HungaryTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("IcelandTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("IndonesiaTop50.csv")
+df12.info()
+df12.describe()
+
+df12 = pd.read_csv("IrelandTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("IsraelTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("ItalyTop50.csv")
+df12.info()
+df12.describe()
+
+df12 = pd.read_csv("JapanTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("LavtiaTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("LithuaniaTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("LuxembourgTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("MalaysiaTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("MexicaTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("NetherlandsTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("NewZealandTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("NicaraguaTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("NorwayTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("PanamaTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("PeruTopTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("PhilippinesTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("PolandTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("PortugalTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("RomaniaTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("SingaporeTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("SlovakiaTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("SouthAfricaTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("SpainTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("SwedenTop50.csv")
+df12.info()
+df12.describe()
+
+
+df12 = pd.read_csv("SwitzerlandTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("TaiwanTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("ThailandTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("TurkeyTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("UnitedKingdomTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("UnitedStatesTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("UruguayTop50.csv")
+df12.info()
+df12.describe()
+
+
+
+df12 = pd.read_csv("VietnamTop50.csv")
+df12.info()
+df12.describe()
+
+
