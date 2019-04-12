43d8fc5fc7222a0d5c4790fa137c177ea9173d8c
Siham Hussein <seehamrun@users.noreply.github.com>
2019-04-11 19:56:56 -0700

    Fill out more of the EDA  (#29)
    * move legend away from box plot
    
    * more eda
    
    * somre more viz
    
    * add some images and pull those in since jupter render is ugly

--- spotify-analysis/visualization.py
+++ spotify-analysis/visualization.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
 import GetTopYearly
 import GetTop50Country
+
+matplotlib.rcParams['figure.figsize'] = [10, 5]
 
 def get_csv(filename):
     return pd.read_csv(filename)
@@ -47,6 +50,27 @@ def makeDanceabilityBarPlot(countrya, countryb):
     plt.legend()
     plt.show()
 
+def make_country_barplot(feature, countries=[]):
+    """
+    Creates a seaborn distribution plot for the loudness of the
+    two countries provided.
+
+    Args:
+        feature(string): the feature to plot
+        countries(list, string): the countries to compare
+    """
+    if len(countries) == 0:
+        countries = GetTop50Country.top_playlists_per_country.keys()
+
+    for country in countries:
+        countrycsv = get_country_csv(country)
+        sns.distplot(countrycsv[feature], label=country)
+
+    plt.title(feature + ' by country')
+    plt.xlabel(feature)
+    if(len(countries) < 10):
+        plt.legend()
+    plt.show()
 
 def makeLoudnessBarplot(countrya, countryb):
     """
@@ -75,30 +99,34 @@ def create_yearly_features_dotplot(featurea, featureb):
         year_pd["year"] = year
         all_data = pd.concat([year_pd, all_data])
         
-    sns.set(style="whitegrid")
+    # idk why but this stripplot function takes 5ever to run - I'm guessing its doing
+    # a group_by underneath the hoods and that is what is causing the execution to
     sns.stripplot(x=featurea, y=featureb, data=all_data, hue="year")
     plt.title("Yearly data " + featurea + ' VS ' + featureb )
-    plt.legend()
+    plt.legend(bbox_to_anchor=(-.05,1.05), loc="upper right")
     plt.show()
 
-def create_country_features_dotplot(featurea, featureb):
+def create_country_features_dotplot(featurea, featureb, countries=[]):
     """
     Creates a dot plot outline of the features provided for all of the yearly data
     """
+    if len(countries) == 0:
+        countries = GetTop50Country.top_playlists_per_country.keys()
+
     all_data = pd.DataFrame()
-    for country in GetTop50Country.top_playlists_per_country:
+    for country in countries:
         country_pd = get_country_csv(country)
         country_pd["country"] = country
         all_data = pd.concat([country_pd, all_data])
         
-    sns.set(style="whitegrid")
     sns.stripplot(x=featurea, y=featureb, data=all_data, hue="country")
     plt.title("Country data " + featurea + ' VS ' + featureb )
-    plt.legend()
+    plt.legend(bbox_to_anchor=(-.05,1.05), loc="upper right")
     plt.show()
 
 if __name__ == "__main__":
+    create_yearly_features_dotplot("danceability", "loudness")
     makeDanceabilityBarPlot("Australia", "UnitedStates")
     makeLoudnessBarplot("Australia", "UnitedStates")
-    create_yearly_features_dotplot("loudness", "danceability")
-    create_country_features_dotplot("loudness", "danceability")
+    make_country_barplot("loudness")
+    create_country_features_dotplot("loudness", "danceability", ["UnitedStates", "Spain", "Sweden", "NewZealand", "Mexica"])
